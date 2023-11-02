7b9ac19b78638860c4b22837147c5aa396296b6b
Jibon <jiboncosta57@gmail.com>
2023-11-02 14:09:54 +0100

    clean up

--- post_processing_scripts/example.js
+++ post_processing_scripts/example.js
@@ -11,3 +11,7 @@ console.log(args);
 /*
  '{"recording_id":"RM_WiTK7pMya8Hc-1698923776909","room_table_id":841,"room_sid":"RM_WiTK7pMya8Hc","file_path":"../recording_files/node_01/RM_WiTK7pMya8Hc/RM_WiTK7pMya8Hc-1698923776909.mp4","file_size":0.32,"recorder_id":"node_01"}'
 */
+
+// you should call process.exit() at the end to make the process finish
+// otherwise the process may not be clear
+process.exit();
