36f1c083b1ea2f3db0aaa17e954f4a106bb96f3d
Jibon <jiboncosta57@gmail.com>
2026-01-28 14:00:02 +0100

    fix(refactor): recorders will use single KV bucket

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/chromedp/cdproto v0.0.0-20250803210736-d308e07a266d
 	github.com/chromedp/chromedp v0.14.2
 	github.com/hashicorp/go-retryablehttp v0.7.8
-	github.com/mynaparrot/plugnmeet-protocol v1.2.0
+	github.com/mynaparrot/plugnmeet-protocol v1.2.1-0.20260128124717-300feb4cdcc7
 	github.com/nats-io/nats.go v1.48.0
 	github.com/sirupsen/logrus v1.9.4
 	google.golang.org/protobuf v1.36.11
@@ -20,10 +20,13 @@ require (
 	buf.build/go/protoyaml v0.6.0 // indirect
 	cel.dev/expr v0.25.1 // indirect
 	github.com/DeRuina/timberjack v1.3.9 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
 	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
 	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/clipperhouse/stringish v0.1.1 // indirect
+	github.com/clipperhouse/uax29/v2 v2.3.0 // indirect
 	github.com/dennwc/iters v1.2.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/frostbyte73/core v0.1.1 // indirect
@@ -36,6 +39,7 @@ require (
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/gofiber/fiber/v2 v2.52.10 // indirect
 	github.com/google/cel-go v0.26.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
@@ -46,7 +50,11 @@ require (
 	github.com/livekit/mageutil v0.0.0-20250511045019-0f1ff63f7731 // indirect
 	github.com/livekit/protocol v1.43.4 // indirect
 	github.com/livekit/psrpc v0.7.1 // indirect
-	github.com/nats-io/nkeys v0.4.12 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/nats-io/jwt/v2 v2.8.0 // indirect
+	github.com/nats-io/nkeys v0.4.14 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pion/datachannel v1.5.10 // indirect
 	github.com/pion/dtls/v3 v3.0.8 // indirect
@@ -68,18 +76,20 @@ require (
 	github.com/redis/go-redis/v9 v9.17.2 // indirect
 	github.com/stoewer/go-strcase v1.3.1 // indirect
 	github.com/twitchtv/twirp v8.1.3+incompatible // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.68.0 // indirect
 	github.com/wlynxg/anet v0.0.5 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.1 // indirect
 	go.uber.org/zap/exp v0.3.0 // indirect
-	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
 	golang.org/x/exp v0.0.0-20251209150349-8475f28825e9 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
-	golang.org/x/sys v0.39.0 // indirect
-	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20251213004720-97cd9d5aeac2 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251213004720-97cd9d5aeac2 // indirect
 	google.golang.org/grpc v1.77.0 // indirect
--- pkg/services/nats/recorder.go
+++ pkg/services/nats/recorder.go
@@ -1,75 +1,69 @@
 package natsservice
 
 import (
-	"errors"
 	"fmt"
 	"time"
 
 	"github.com/mynaparrot/plugnmeet-protocol/plugnmeet"
-	"github.com/nats-io/nats.go/jetstream"
+	"github.com/mynaparrot/plugnmeet-protocol/utils"
 )
 
-const (
-	RecorderKvBucket = "%s-%s"
-)
-
+// AddRecorder initializes a recorder's information in the consolidated KV bucket.
 func (s *NatsService) AddRecorder() error {
-	bucket := fmt.Sprintf(RecorderKvBucket, s.app.NatsInfo.Recorder.RecorderInfoKv, s.app.Recorder.Id)
-	kv, err := s.js.CreateOrUpdateKeyValue(s.ctx, jetstream.KeyValueConfig{
-		Replicas: s.app.NatsInfo.NumReplicas,
-		Bucket:   bucket,
-	})
+	kv, err := s.js.KeyValue(s.ctx, s.app.NatsInfo.Recorder.RecorderInfoKv)
 	if err != nil {
 		return err
 	}
 
+	recorderId := s.app.Recorder.Id
+	maxLimitField := fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_MAX_LIMIT)
+	progressField := fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_CURRENT_PROGRESS)
+	lastPingField := fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_LAST_PING)
+
+	// Prepare data using the new key format from the utils package
 	data := map[string]string{
-		fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_MAX_LIMIT):        fmt.Sprintf("%d", s.app.Recorder.MaxLimit),
-		fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_CURRENT_PROGRESS): "0",
-		fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_LAST_PING):        fmt.Sprintf("%d", time.Now().UTC().UnixMilli()),
+		utils.FormatRecorderKey(recorderId, maxLimitField): fmt.Sprintf("%d", s.app.Recorder.MaxLimit),
+		utils.FormatRecorderKey(recorderId, progressField): "0",
+		utils.FormatRecorderKey(recorderId, lastPingField): fmt.Sprintf("%d", time.Now().UTC().UnixMilli()),
 	}
 
+	// Put all fields into the consolidated bucket
 	for k, v := range data {
 		_, err = kv.PutString(s.ctx, k, v)
 		if err != nil {
-			s.logger.WithError(err).Errorln("failed to add recorder info")
+			s.logger.WithError(err).Errorln("failed to add recorder info field")
 		}
 	}
 
 	return nil
 }
 
+// UpdateLastPing updates the last ping timestamp for a specific recorder in the consolidated KV bucket.
 func (s *NatsService) UpdateLastPing() error {
-	bucket := fmt.Sprintf(RecorderKvBucket, s.app.NatsInfo.Recorder.RecorderInfoKv, s.app.Recorder.Id)
-	kv, err := s.js.KeyValue(s.ctx, bucket)
-	switch {
-	case errors.Is(err, jetstream.ErrBucketNotFound):
-		return fmt.Errorf("this recorder was not found")
-	case err != nil:
-		return err
-	}
-
-	_, err = kv.PutString(s.ctx, fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_LAST_PING), fmt.Sprintf("%d", time.Now().UTC().UnixMilli()))
+	kv, err := s.js.KeyValue(s.ctx, s.app.NatsInfo.Recorder.RecorderInfoKv)
 	if err != nil {
-		return err
+		return fmt.Errorf("recorder info bucket not found: %w", err)
 	}
 
-	return nil
+	recorderId := s.app.Recorder.Id
+	lastPingField := fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_LAST_PING)
+	key := utils.FormatRecorderKey(recorderId, lastPingField)
+
+	_, err = kv.PutString(s.ctx, key, fmt.Sprintf("%d", time.Now().UTC().UnixMilli()))
+	return err
 }
 
+// UpdateCurrentProgress updates the current progress for a specific recorder in the consolidated KV bucket.
 func (s *NatsService) UpdateCurrentProgress(progress int) error {
-	bucket := fmt.Sprintf(RecorderKvBucket, s.app.NatsInfo.Recorder.RecorderInfoKv, s.app.Recorder.Id)
-	kv, err := s.js.KeyValue(s.ctx, bucket)
-	switch {
-	case errors.Is(err, jetstream.ErrBucketNotFound):
-		return fmt.Errorf("this recorder was not found")
-	case err != nil:
-		return err
+	kv, err := s.js.KeyValue(s.ctx, s.app.NatsInfo.Recorder.RecorderInfoKv)
+	if err != nil {
+		return fmt.Errorf("recorder info bucket not found: %w", err)
 	}
 
-	key := fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_CURRENT_PROGRESS)
-	newValue := fmt.Sprintf("%d", progress)
+	recorderId := s.app.Recorder.Id
+	progressField := fmt.Sprintf("%d", plugnmeet.RecorderInfoKeys_RECORDER_INFO_CURRENT_PROGRESS)
+	key := utils.FormatRecorderKey(recorderId, progressField)
 
-	_, err = kv.PutString(s.ctx, key, newValue)
+	_, err = kv.PutString(s.ctx, key, fmt.Sprintf("%d", progress))
 	return err
 }
